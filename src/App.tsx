33b744cccf42424ab73400bdf7527b731e9a0a63
Lê Công Định <hobao3210@gmail.com>
2022-03-16 11:13:24 +0700

    commit router page

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,53 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import { Product } from './types/product'
 import ShowInfor from './component/ShowInfor'
+import { list } from './api/product'
+import { Navigate, NavLink, Routes, Route } from "react-router-dom"
+import WebsiteLayout from './page/layouts/WebsiteLayout'
+import HomePage from './page/homePage'
+import ProductPage from './page/ProductPage'
+import AdminLayout from './page/layouts/AdminLayout'
+import Dashboard from './page/Dashboard'
+import ProductManager from './page/ProductManager'
+import ProductDetail from './page/ProductDetail'
 
 function App() {
-  const [count, setCount] = useState<number>(0)
-  const [info, setInfo] = useState<Product>({
-    name: "Định",
-    age: 11
-  })
-  return (
-    <div className="App">
-      {count} <button onClick={() => setCount(count + 1)}>plussssss</button>
-      <ShowInfor person={info} />
+  const [products, setProducts] = useState<{ _id: number, name: String }[]>([]);
 
-      <h1>Tên: {info.name}</h1>
-      <h2>Tuổi: {info.age}</h2>
+  useEffect(() => {
+    const getProducts = async () => {
+      const { data } = await list();
+      setProducts(data);
+    }
+    getProducts();
+  }, [])
+  return (
+    <div className='App'>
+      <header>
+        <ul>
+          <li><NavLink to='/'>Home Page</NavLink></li>
+          <li><NavLink to='/product'>Products</NavLink></li>
+          <li><NavLink to='/about'>About</NavLink></li>
+        </ul>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<WebsiteLayout />} >
+            <Route index element={<HomePage />} />
+            <Route path='product'>
+              <Route index element={<ProductPage />} />
+              <Route path=':id' element={<ProductDetail />} />
+            </Route>
+          </Route>
+          <Route path='admin' element={<AdminLayout />}>
+            <Route index element={<Navigate to="/admin/dashboard" />} />
+            <Route path='dashboard' element={<Dashboard />} />
+            <Route path='product' element={<ProductManager />} />
+          </Route>
+        </Routes>
+      </main>
     </div>
   )
 }
